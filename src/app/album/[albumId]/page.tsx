82aa25ae6547dc148346381e863ef42250bcc4e6
LiuHenry9800 <henryliu9800@gmail.com>
2024-02-18 05:01:20 -0800

    Component

--- src/app/album/[albumId]/page.tsx
+++ src/app/album/[albumId]/page.tsx
@@ -20,6 +20,7 @@ import Grid from "@mui/material/Grid";
 import { api } from "~/trpc/server";
 import { getServerAuthSession } from "~/server/auth";
 import { TextField, Typography, Button, Box, Rating } from '@mui/material';
+import NavsComponent from "../../_components/NavsComponent";
 
 import AlbumReviewForm from "./reviewform";
 
@@ -41,35 +42,7 @@ export default async function AlbumInfoPage({
   className="flex-1 bg-gray-200 flex flex-row items-start justify-between pt-[37px] pb-[35px] pr-[30px] pl-[75px] box-border gap-[20px] max-w-full z-[1] text-left text-5xl text-white font-lexend mq750:pl-[37px] mq750:box-border"
 
 >
-  <div className="w-[1013px] flex flex-row items-center justify-start gap-[74px] max-w-full mq1050:gap-[74px] mq750:gap-[74px]">
-    <div className="w-[335px] flex flex-row items-start justify-start gap-[25px] max-w-full">
-      <img
-        className="h-[68px] w-[76px] relative object-cover"
-        alt=""
-        src="/appicon@2x.png"
-      />
-      <div className="h-14 flex-1 flex flex-col items-start justify-start pt-[3px] px-0 pb-0 box-border">
-        <img
-          className="self-stretch flex-1 relative max-w-full overflow-hidden max-h-full"
-          alt=""
-        />
-      </div>
-    </div>
-    <div className="flex-1 flex flex-col items-start justify-start pt-0 px-0 pb-[3px] box-border max-w-full">
-      <div className="self-stretch rounded-xl bg-darkslategray-100 flex flex-row items-center justify-start py-[15px] px-10 box-border gap-[12px] max-w-full">
-        <img
-          className="h-[23px] w-[23px] relative overflow-hidden shrink-0"
-          alt=""
-          src="/empty-space.svg"
-        />
-        <input
-          className="w-[325px] [border:none] [outline:none] font-lexend text-xl bg-[transparent] h-[25px] relative text-white text-left inline-block max-w-full whitespace-nowrap"
-          placeholder="Search for albums, artists, users..."
-          type="text"
-        />
-      </div>
-    </div>
-  </div>
+  <NavsComponent/>
   <div className="w-[239px] flex flex-col items-start justify-start pt-[5px] px-0 pb-0 box-border">
     <div className="self-stretch shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] flex flex-row items-center justify-start pt-2 pb-[9px] pr-0 pl-[26px] relative gap-[16px]">
       <img
