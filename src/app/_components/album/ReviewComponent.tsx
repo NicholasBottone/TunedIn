70d6f6a5b90531e8e7e7c0e4719b5a399b273cfc
Nicholas Bottone <nick@bottone.io>
2024-02-18 02:31:07 -0800

    Fix lint errors

--- src/app/_components/album/ReviewComponent.tsx
+++ src/app/_components/album/ReviewComponent.tsx
@@ -16,57 +16,57 @@ const ReviewComponent: FunctionComponent<ReviewComponentType> = ({
 
   return (
     <div
-      className="self-stretch h-[265px] relative bg-midnightblue overflow-hidden shrink-0 max-w-full text-left text-5xl text-white font-lexend"
+      className="relative h-[265px] max-w-full shrink-0 self-stretch overflow-hidden bg-midnightblue text-left font-lexend text-5xl text-white"
       style={reviewComponentStyle}
     >
-      <div className="absolute top-[3px] left-[0px] w-[993px] flex flex-col items-start justify-start gap-[262px]">
-        <div className="self-stretch h-px relative box-border border-t-[1px] border-solid border-white" />
-        <div className="self-stretch h-px relative box-border border-t-[1px] border-solid border-white" />
+      <div className="absolute left-[0px] top-[3px] flex w-[993px] flex-col items-start justify-start gap-[262px]">
+        <div className="relative box-border h-px self-stretch border-t-[1px] border-solid border-white" />
+        <div className="relative box-border h-px self-stretch border-t-[1px] border-solid border-white" />
       </div>
       <img
-        className="absolute top-[27px] left-[26px] rounded-[50%] w-[93px] h-[92px] object-cover z-[1]"
+        className="absolute left-[26px] top-[27px] z-[1] h-[92px] w-[93px] rounded-[50%] object-cover"
         loading="eager"
         alt=""
         src="/user-pfp@2x.png"
       />
-      <div className="absolute top-[38px] left-[170px] lowercase font-semibold inline-block w-[471px] h-[47px] z-[1] mq450:text-lgi">
+      <div className="absolute left-[170px] top-[38px] z-[1] inline-block h-[47px] w-[471px] font-semibold lowercase mq450:text-lgi">
         Jimmy neutron
       </div>
-      <div className="absolute top-[85px] right-[64px] w-[721px] flex flex-row flex-wrap items-center justify-center pt-[21px] pb-[22px] pr-0 pl-2.5 box-border max-w-full z-[1] text-xl">
-        <div className="h-[100px] flex-1 relative lowercase font-light inline-block max-w-full mq450:text-base">
-          Image Tracer allows you to "trace" an image and generate an exact copy
-          as a vector shape. This is perfect for removing a background, editing
-          the shape/color, or export as an SVG. I often use this when creating
-          illustrations or modifying a logo or icon.
+      <div className="absolute right-[64px] top-[85px] z-[1] box-border flex w-[721px] max-w-full flex-row flex-wrap items-center justify-center pb-[22px] pl-2.5 pr-0 pt-[21px] text-xl">
+        <div className="relative inline-block h-[100px] max-w-full flex-1 font-light lowercase mq450:text-base">
+          Image Tracer allows you to &quot;trace&quot; an image and generate an
+          exact copy as a vector shape. This is perfect for removing a
+          background, editing the shape/color, or export as an SVG. I often use
+          this when creating illustrations or modifying a logo or icon.
         </div>
       </div>
-      <div className="absolute top-[38px] left-[601px] flex flex-row items-start justify-start py-0 pr-px pl-0 gap-[7px] z-[2]">
+      <div className="absolute left-[601px] top-[38px] z-[2] flex flex-row items-start justify-start gap-[7px] py-0 pl-0 pr-px">
         <img
-          className="h-[46.1px] w-[46.1px] relative min-h-[46px]"
+          className="relative h-[46.1px] min-h-[46px] w-[46.1px]"
           loading="eager"
           alt=""
           src="/star-6.svg"
         />
         <img
-          className="h-[46.1px] w-[46.1px] relative min-h-[46px]"
+          className="relative h-[46.1px] min-h-[46px] w-[46.1px]"
           loading="eager"
           alt=""
           src="/star-5.svg"
         />
         <img
-          className="h-[46.1px] w-[46.1px] relative min-h-[46px]"
+          className="relative h-[46.1px] min-h-[46px] w-[46.1px]"
           loading="eager"
           alt=""
           src="/star-4.svg"
         />
         <img
-          className="h-[46.1px] w-[46.1px] relative min-h-[46px]"
+          className="relative h-[46.1px] min-h-[46px] w-[46.1px]"
           loading="eager"
           alt=""
           src="/star-3.svg"
         />
         <img
-          className="h-[46.1px] w-[46.1px] relative min-h-[46px]"
+          className="relative h-[46.1px] min-h-[46px] w-[46.1px]"
           loading="eager"
           alt=""
           src="/star-2.svg"
--- src/app/album/[albumId]/page.tsx
+++ src/app/album/[albumId]/page.tsx
@@ -1,85 +1,82 @@
-import React from 'react'
+import React from "react";
 
+import Box from "@mui/material/Box";
 
-import Box from '@mui/material/Box';
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
 
-import Container from '@mui/material/Container';
-import Stack from '@mui/material/Stack';
+import Divider from "@mui/material/Divider";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
 
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Grid from "@mui/material/Grid";
+import { api } from "~/trpc/server";
+import { getServerAuthSession } from "~/server/auth";
 
-import Divider from '@mui/material/Divider';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
+import AlbumReviewForm from "./reviewform";
 
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Grid from '@mui/material/Grid';
-import { api } from '~/trpc/server';
-import { getServerAuthSession } from '~/server/auth';
+export default async function AlbumInfoPage({
+  params,
+}: {
+  params: { albumId: string };
+}) {
+  const album = await api.album.getAlbumById.query({ id: params.albumId });
+  const session = await getServerAuthSession();
 
-import AlbumReviewForm from './reviewform';
+  const reviews = await api.review.getReviewsByAlbumId.query({
+    albumId: params.albumId,
+  });
 
-
-
-
-
-
-export default async function AlbumInfoPage({params}:{params: {albumId:string}}) {
-
-    const album = await api.album.getAlbumById.query({id:params.albumId})
-    const session = await getServerAuthSession()
-
-    
-
-    const reviews = await api.review.getReviewsByAlbumId.query({albumId:params.albumId})
-
-
-    
-
-    console.log(album?.image)
-    return (
-      <Box style={{ backgroundColor: "#391F67"}}>
-
+  console.log(album?.image);
+  return (
+    <Box style={{ backgroundColor: "#391F67" }}>
       <Stack
-        sx={{ height: '100vh', width: "100vw" }}
+        sx={{ height: "100vh", width: "100vw" }}
         direction="row"
         divider={<Divider orientation="vertical" flexItem />}
         spacing={2}
       >
-        <Box sx={{ color: "#FFFFFF", height: '100vh', width: "30vw", }}>
+        <Box sx={{ color: "#FFFFFF", height: "100vh", width: "30vw" }}>
           <img
-            className="self-stretch flex-1 relative max-w-full overflow-hidden max-h-full"
+            className="relative max-h-full max-w-full flex-1 self-stretch overflow-hidden"
             alt=""
-            src={album!.image ?? undefined}
-            style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'cover' }}
+            src={album.image ?? undefined}
+            style={{ maxWidth: "100%", maxHeight: "100%", objectFit: "cover" }}
           />
           <Typography variant="h3" component="h2">
             Track List
           </Typography>
-          <AlbumReviewForm/>
+          <AlbumReviewForm />
           <List>
-            {album?.tracks.map((track)=>{
-
-              return (<ListItem>
-                <ListItemText primary={track} style={{ color: "#FFFFFF" }} />
-              </ListItem>);
-
+            {album?.tracks.map((track) => {
+              return (
+                <ListItem key={track}>
+                  <ListItemText primary={track} style={{ color: "#FFFFFF" }} />
+                </ListItem>
+              );
             })}
-            
           </List>
         </Box>
-        <Box sx={{ color: "#FFFFFF", height: '100vh', width: "70vw", padding: '20px' }}>
-
+        <Box
+          sx={{
+            color: "#FFFFFF",
+            height: "100vh",
+            width: "70vw",
+            padding: "20px",
+          }}
+        >
           <Stack
-            sx={{ height: '100vh', width: "100vw" }}
+            sx={{ height: "100vh", width: "100vw" }}
             direction="column"
             divider={<Divider orientation="horizontal" flexItem />}
             spacing={2}
@@ -88,37 +85,36 @@ export default async function AlbumInfoPage({params}:{params: {albumId:string}})
               {album?.name}
             </Typography>
             <Typography variant="h4" component="h2">
-              {"Artist: "+album?.artist}
+              {"Artist: " + album?.artist}
             </Typography>
             <Typography variant="h4" component="h2">
-              {"Release Date: "+album?.releaseDate}
+              {"Release Date: " + album?.releaseDate}
             </Typography>
 
             <List>
-            {reviews?.map((review)=>{
-
-              return (
-                <Box display="flex" marginBottom="16px">
-                  <Avatar alt={session?.user?.name ?? undefined} src={session?.user.image ?? undefined} />
-                  <div>
-                    <Typography variant="subtitle1">{session?.user.name}</Typography>
-                    <Typography>{review.rating}</Typography>
-                    <Typography fontStyle="italic">{review.message}</Typography>
-                  </div>
-                </Box>
-              )
-
-            })}
-            
-          </List>
-
-
+              {reviews?.map((review) => {
+                return (
+                  <Box display="flex" marginBottom="16px" key={review.id}>
+                    <Avatar
+                      alt={session?.user?.name ?? undefined}
+                      src={session?.user.image ?? undefined}
+                    />
+                    <div>
+                      <Typography variant="subtitle1">
+                        {session?.user.name}
+                      </Typography>
+                      <Typography>{review.rating}</Typography>
+                      <Typography fontStyle="italic">
+                        {review.message}
+                      </Typography>
+                    </div>
+                  </Box>
+                );
+              })}
+            </List>
           </Stack>
-
         </Box>
       </Stack>
-    
     </Box>
-    
-  )
+  );
 }
