21706a36bf1273567392f04bd1c89a1857966234
LiuHenry9800 <henryliu9800@gmail.com>
2024-02-17 23:58:11 -0800

    album

--- src/app/album/[albumId]/page.tsx
+++ src/app/album/[albumId]/page.tsx
@@ -11,8 +11,28 @@ import Divider from '@mui/material/Divider';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 
-export default function AlbumInfoPage({params}:{params: {albumId:string}}) {
-  return (
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import { api } from '~/trpc/server';
+
+
+
+
+
+export default async function AlbumInfoPage({params}:{params: {albumId:string}}) {
+
+    const album = await api.album.getAlbumById.query({id:params.albumId})
+    console.log(album?.image)
+    return (
     
     <Box style={{backgroundColor:"#391F67",display:"flex"}}>
       
@@ -24,10 +44,24 @@ export default function AlbumInfoPage({params}:{params: {albumId:string}}) {
         spacing={2}
       >
         <Box sx={{ color:"#391F67", height: '100vh',width:"30vw" }}>
-        
+        <img
+              className="self-stretch flex-1 relative max-w-full overflow-hidden max-h-full"
+              alt=""
+              src={album.image}
+        />
         <Typography variant="h1" component="h2" color={"#FFFFFF"}>
         Track List
         </Typography>
+        <List>
+              {
+                <ListItem>
+                  <ListItemText
+                    style={{color:"#FFFFFF"}}
+                    primary="Single-line item"
+                  />
+                </ListItem>
+              }
+            </List>
         </Box>
         <Box sx={{ color:"#391F67", height: '100vh',width:"70vw" }}>
         <Typography variant="h1" component="h2" color={"#FFFFFF"}>
